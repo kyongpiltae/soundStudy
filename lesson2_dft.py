e73cccf61ada6f487db3ef9e10eaab1783f8727c
kyongpiltae <kyongpiltae@gmail.com>
2020-03-06 00:09:59 +0900

    0306_2

---
+++ lesson2_dft.py
@@ -0,0 +1,89 @@
+#%% 
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%% 
+N= 64 
+k0 =7
+x = np.exp(1j * 2*np.pi * k0/N* np.arange(N))
+X = np.array([])
+for k in range(N):
+    s = np.exp(1j *2 * np.pi *k / N * np.arange(N))
+    X = np.append(X,sum(x*np.conjugate(s)))
+print(f"input x {x.shape} , sine {s.shape} , Output X {X.shape} ")
+
+# %%
+plt.plot(np.arange(N),abs(X))
+plt.axis([0,N-1,0,N])
+plt.show()
+
+# %%
+# real signal which has 2 complex signal.
+N= 64 
+k0 =7
+x = np.cos( 2*np.pi * k0/N* np.arange(N))
+X = np.array([])
+nv = np.arange(-N/2,N/2)
+
+for k in range(N):
+    s = np.exp(1j *2 * np.pi *k / N * np.arange(N))
+    X = np.append(X,sum(x*np.conjugate(s)))
+print(f"input x {x.shape} , sine {s.shape} , Output X {X.shape} ")
+plt.plot(np.arange(N),abs(X))
+plt.axis([0,N-1,0,N])
+plt.show()
+
+# %%
+N= 64 
+k0 =7
+x = np.cos( 2*np.pi * k0/N* np.arange(N))
+X = np.array([])
+nv = np.arange(-N/2,N/2)
+kv = np.arange(-N/2,N/2)
+for k in kv:
+    s = np.exp(1j *2 * np.pi *k / N * np.arange(N))
+    X = np.append(X,sum(x*np.conjugate(s)))
+print(f"input x {x.shape} , sine {s.shape} , Output X {X.shape} ")
+plt.plot(kv,abs(X))
+plt.axis([-N/2,N/2-1,0,N])
+plt.show()
+# %%
+N= 64 
+k0 =7.5
+x = np.cos( 2*np.pi * k0/N* np.arange(N))
+X = np.array([])
+nv = np.arange(-N/2,N/2)
+kv = np.arange(-N/2,N/2)
+for k in kv:
+    s = np.exp(1j *2 * np.pi *k / N *nv)
+    X = np.append(X,sum(x*np.conjugate(s)))
+
+
+print(f"input x {x.shape} , sine {s.shape} , Output X {X.shape} ")
+plt.plot(kv,abs(X))
+plt.axis([-N/2,N/2-1,0,N])
+plt.show()
+
+# %%
+N= 64 
+k0 =7.5
+x = np.cos( 2*np.pi * k0/N* np.arange(N))
+X = np.array([])
+nv = np.arange(-N/2,N/2)
+kv = np.arange(-N/2,N/2)
+for k in kv:
+    s = np.exp(1j *2 * np.pi *k / N * nv)
+    X = np.append(X,sum(x*np.conjugate(s)))
+
+
+y = np.array([])
+for n in nv:
+    s = np.exp(1j *2 * np.pi * n / N * kv)
+    y = np.append(y,1.0/N *sum(X*s))
+print(f"input x {x.shape} , sine {s.shape} , Output X {X.shape} ")
+plt.plot(kv,y)
+plt.axis([-N/2,N/2-1,-1,1])
+plt.show()
+
+
+# %%
